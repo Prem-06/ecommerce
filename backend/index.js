11abc853127ff68eb73171e0a31c2745e14173d2
Prem Prakash Pal <0610prem@gmail.com>
2024-04-05 10:22:58 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const express=require('express')
 const app=express();
 const mongoose=require('mongoose')
-const port=3000;
+const port=process.env.PORT||3000;
 const cors=require('cors')
-const uri='mongodb+srv://06prempal:5xkeCIgX9XwyK7mK@cluster0.c11eds1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-mongoose.connect('mongodb://127.0.0.1:27017/intern').then(()=>{
+const uri=process.env.DB_URL;
+mongoose.connect(uri).then(()=>{
     console.log('connected to database')
 }).catch((err)=>{
     console.log(err)
@@ -13,6 +13,9 @@ app.use(cors())
 app.use(express.json())
 require('./model/product_detail')
 require('./model/checkout_model')
+app.get('/',(req,res)=>{
+    res.send("server is working")
+})
 app.use(require('./routes/setdata'))
 app.use(require('./routes/getdata'))
 app.use(require('./routes/place_order'))
